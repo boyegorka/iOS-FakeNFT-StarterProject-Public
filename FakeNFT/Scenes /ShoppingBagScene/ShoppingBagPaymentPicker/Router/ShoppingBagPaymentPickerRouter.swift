0af5a76622bd84a55c6af061398d70d8b95be52e
Олег Аксененко <aksenenko_oyu@drom.ru>
2023-10-15 23:11:08 +1000

    YP-7: Добавлен показ и старт загрузки WebView

--- FakeNFT/Scenes /ShoppingBagScene/ShoppingBagPaymentPicker/Router/ShoppingBagPaymentPickerRouter.swift
+++ FakeNFT/Scenes /ShoppingBagScene/ShoppingBagPaymentPicker/Router/ShoppingBagPaymentPickerRouter.swift
@@ -13,10 +13,23 @@ protocol ShoppingBagPaymentPickerRouter {
 
 final class ShoppingBagPaymentPickerRouterImpl {
     var viewController: UIViewController?
+    var webViewController: WebViewControllerInput?
 }
 
 extension ShoppingBagPaymentPickerRouterImpl: ShoppingBagPaymentPickerRouter {
     func presentWebView(with url: URL) {
-        // TODO: сделать открытие webViewController-а
+        let webViewController = WebViewController(with: url, output: self)
+        self.webViewController = webViewController
+        viewController?.navigationController?.pushViewController(webViewController, animated: true)
+    }
+}
+
+extension ShoppingBagPaymentPickerRouterImpl: WebViewControllerOutput {
+    func viewDidLoad() {
+        webViewController?.startLoading()
+    }
+
+    func didTapBackButton() {
+        viewController?.navigationController?.popViewController(animated: true)
     }
 }
